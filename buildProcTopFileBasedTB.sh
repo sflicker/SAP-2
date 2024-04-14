9093810c453009f8bd28b84b61f54b4b71629871
Scott Flicker <sflicker2@earthlink.net>
2024-04-14 17:48:14 -0400

    work in progress

--- buildProcTopFileBasedTB.sh
+++ buildProcTopFileBasedTB.sh
@@ -47,7 +47,7 @@ ghdl -e --std=08 -fsynopsys -v proc_top_filebased_tb
 echo "Running Test Bench"
 #run
 ghdl -r --std=08 -fsynopsys -v proc_top_filebased_tb -gfile_name="test_program_1.txt" --stop-time=100000ns --vcd=proc_top_filebased_tb.vcd
-ghdl -r --std=08 -fsynopsys -v proc_top_filebased_tb -gfile_name="load_accumulator_test.txt" --stop-time=10000ns --vcd=load_accumulator_test.vcd
+ghdl -r --std=08 -fsynopsys -v proc_top_filebased_tb -gfile_name="load_accumulator_test.txt" --stop-time=20000ns --vcd=load_accumulator_test.vcd
 
 
 
